72e9601198323d5e42c61255a401c66453dc73af
githmith <vuminhthanh2305@gmail.com>
2017-02-26 02:09:32 -0500

    Adjust script to detect faces and move images

--- scripts/thanh_face_detection.py
+++ scripts/thanh_face_detection.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import glob
+import shutil
 # from matplotlib import pyplot as plt
 
 def detectAndDisplay3(imgName):
@@ -83,44 +85,46 @@ def detect(imgName):
 
 	img = cv2.imread(imgName)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	cv2.equalizeHist(gray, gray) # helps improve the detection
 
-	faces = face_cascade.detectMultiScale3(gray, 1.05, 5, outputRejectLevels=True)
-	if len(faces) == 0:
-		print(imgName)
-
-def detectAndReturn(imgName):
-	face_cascade = cv2.CascadeClassifier('openCVDetector/haarcascade_frontalface_default.xml')
-
-	img = cv2.imread(imgName)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+	# return a list of detection boxes
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-	print(len(faces))
 	return len(faces)
 
 # =================================== MAIN ================================== #
 src = "/Users/thanhvu/Desktop/FaceSimilarity/faces/"
 # out = open("bad_imgs.txt","w+")
 
-# cnt_bad = 0
-# for i in range(26):	
-# 	imgName = src + str(i).zfill(6) + ".png"
-# 	faces = detectAndReturn(imgName)
-# 	print(imgName)
-# 	if faces == 0:
-# 		out.write(imgName+"\n")
-# 		cnt_bad += 1
-# print("cnt bad: %d" % (cnt_bad))
+# all names from Names100Dataset. Each have 800 images
+# names = ['Aaron', 'Abby', 'Amanda', 'Andrea', 'Angela', 'Ann', 'Anna', 'Annie', 'Anthony', 'Barbara', 'Brandon', 'Brian', 'Chris', 'Christina', 'Christine', 'Cindy', 'Danielle', 'Danny', 'David', 'Dylan', 'Elena', 'Elizabeth', 'Emily', 'Emma', 'Eric', 'Erin', 'Eva', 'Evan', 'Gabriel', 'Gary', 'Grace', 'Greg', 'Helen', 'Ian', 'Ivan', 'Jackie', 'Jacob', 'Jake', 'James', 'Jamie', 'Jane', 'Jason', 'Jeff', 'Jesse', 'Jim', 'Jo', 'Joel', 'Joey', 'Jon', 'Jonathan', 'Joseph', 'Julie', 'Kate', 'Katie', 'Kelly', 'Kevin', 'Kim', 'Kyle', 'Lauren', 'Leah', 'Linda', 'Lisa', 'Lucas', 'Maggie', 'Marc', 'Matthew', 'Melissa', 'Michelle', 'Mike', 'Monica', 'Nancy', 'Natalia', 'Nathan', 'Nick', 'Nicolas', 'Noah', 'Oliver', 'Olivia', 'Patricia', 'Patrick', 'Paula', 'Rachel', 'Rebecca', 'Rick', 'Samantha', 'Sarah', 'Sergio', 'Sofia', 'Stephanie', 'Stephen', 'Steve', 'Steven', 'Sue', 'Thomas', 'Tina', 'Tony', 'Tyler', 'Vanessa', 'Victor', 'Zoe']
 
-# for i in range(0,1):	
-	# imgName = src + str(i).zfill(6) + ".png"
-	# detectAndDisplay2(imgName)
-	# print(imgName)
-	# if faces == 0:
-		# out.write(imgName+"\n")
-# 		cnt_bad += 1
-# print("cnt bad: %d" % (cnt_bad))
+# imgNames = []
+# for name in names:
+# 	for j in range(1,801):
+# 		imgName = "../../Names100Dataset/Names100_Images/"+name+"_"+ str(j)+".png"
+# 		imgNames.append(imgName)
+# print(len(imgNames))
 
-imgName = "../test/face_sim_test.jpg"
-print(imgName)
-detectAndDisplay(imgName)
+# ======== Test individual images ======== #
+# imgName = "../test/face_sim_test.jpg"
+# print(imgName)
+# detectAndDisplay(imgName)
+
+# ======== Detect and move images ======== #
+good = 0
+# for i,imgName in enumerate(imgNames):
+for i,imgName in enumerate(sorted(glob.glob("../../Names100Dataset/Names100_Images/*"))):
+	print (imgName)
+	faces = detect(imgName)
+	if faces != 0:
+		good+= 1
+		shutil.move(imgName,imgName.replace("Names100_Images","good"))
+	else:
+		shutil.move(imgName,imgName.replace("Names100_Images","bad"))
+
+	print("Current index: "+str(i))
+	print("Number of good images: "+str(good))
+	if good == 100:
+		break
+print("total good images: %d" % (good))
+
